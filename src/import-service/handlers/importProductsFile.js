968a1e371cdb613785a7314e6716c418f7130a13
ArtiFisher <artemis.fisher@gmail.com>
2020-11-10 18:15:59 +0300

    feat: Add handler for signed URL getter

--- src/import-service/handlers/importProductsFile.js
+++ src/import-service/handlers/importProductsFile.js
@@ -1,13 +1,22 @@
 import { StatusCodes } from 'http-status-codes';
+import AWS from 'aws-sdk';
 import { headers } from '../../utils/const.js';
+
+const S3 = new AWS.S3({ apiVersion: '2006-03-01', region: 'us-east-1' });
 
 export const handler = async event => {
   let body;
   let statusCode;
   console.log(event);
   try {
+    const params = {
+      Key: `uploaded/${event.pathParameters?.name}`,
+      Bucket: 'imported-products',
+      Expires: 60,
+      ContentType: 'text/csv'
+    }
+    body = await S3.getSignedUrlPromise('putObject', params);
     statusCode = StatusCodes.OK;
-    body = JSON.stringify({ message: 'Ok' });
   } catch(e) {
     body = JSON.stringify(e);
     statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
