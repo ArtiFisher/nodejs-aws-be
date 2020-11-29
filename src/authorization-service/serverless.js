66ae8e71ed07a7ea0c0a371de95049fb4f61c850
ArtiFisher <artemis.fisher@gmail.com>
2020-11-29 14:26:17 +0300

    refactor: Change serverless config file

---
+++ src/authorization-service/serverless.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  service: {
+    name: 'authorization-service'
+  },
+  frameworkVersion: '2',
+  plugins: ['serverless-dotenv-plugin', 'serverless-webpack'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    region: 'eu-west-1',
+  },
+  functions: {
+    basicAuthorizer: {
+      handler: 'handlers/basicAuthorizer.handler'
+    },
+  },
+  resources: {},
+};
